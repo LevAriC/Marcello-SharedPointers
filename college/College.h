ac1d92d07ed5c730bcb75927cc8549ecac4c375f
Miki <makmel.miki@gmail.com>
2018-05-01 20:51:46 +0300

    Still shit
    Added header college
    Added Main.cpp

--- college/College.h
+++ college/College.h
@@ -1,6 +1,7 @@
 #ifndef COLLEGE_H
 #define COLLEGE_H
 #include "Department.h"
+#include <fstream>
 
 class College
 {
@@ -15,4 +16,32 @@ public:
 	Student* getListOfStudents() const;
 };
 
+
+void read_init()
+{
+	ifstream inputFile("init.txt");
+	char buffer[1024];
+	College newCollege;
+	string newCollegeName;
+
+	if (inputFile.is_open())
+	{
+		while (!inputFile.eof())
+		{
+			inputFile.getline(buffer, 1024, ':');
+			if (buffer[0] == '#')
+			{
+				inputFile.getline(buffer, 1024, '\n');
+				continue;
+			}
+			inputFile.getline(buffer, 1024, '\n');
+			strcpy(&newCollegeName[0], buffer);
+		
+
+		}
+	}
+
+	inputFile.close();
+	cout << "Reading from file is complete!" << endl << endl;
+}
 #endif
